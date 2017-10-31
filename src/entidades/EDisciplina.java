fc13b785b2c1e1893b778ec83f58806585908e23
gguui <gguui@192.168.200.66>
2017-10-31 20:35:58 -0200

    Finalização > Entidade Disciplina

--- src/entidades/EDisciplina.java
+++ src/entidades/EDisciplina.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
-/**
- *
- * @author gguui
- */
+import util.ANOLETIVO;
+
 public class EDisciplina {
-    
+    private int codigo;
+    private String descricao;
+    private ANOLETIVO anoLetivo;
+    private int cargaHoraria;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public ANOLETIVO getAnoLetivo() {
+        return anoLetivo;
+    }
+
+    public void setAnoLetivo(ANOLETIVO anoLetivo) {
+        this.anoLetivo = anoLetivo;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 }
